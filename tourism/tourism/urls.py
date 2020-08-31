8b567591c170d0e0b305361c93331074639abb2d
kamula <kamulaisaac@gmail.com>
2020-08-31 21:31:03 +0300

    user registration api documentation

--- tourism/tourism/urls.py
+++ tourism/tourism/urls.py
@@ -16,7 +16,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+#end
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Users api",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/account/",include('users.urls'))
+    path("api/account/",include('users.urls')),
+    
+    path("api/docs/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
 ]
+
+
--- tourism/users/views.py
+++ tourism/users/views.py
@@ -2,7 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 from .serializers import userSerializer
+
 
 @api_view(['POST',])
 def registration_view(request):
