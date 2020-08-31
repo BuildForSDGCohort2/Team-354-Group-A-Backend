5a89ec79310c06dc4970f8671344659dd28f05f8
kamula <kamulaisaac@gmail.com>
2020-08-31 16:40:53 +0300

    user registration api route

--- tourism/users/views.py
+++ tourism/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from users.api.serializers import userSerializer
+
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = userSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            user = serializer.save()            
+            data["email"] = user.email
+            data["first_name"] = user.first_name
+            data["last_name"] = user.last_name
+            data["phone_number"] = user.phone_number
+            
+        else:
+            data = serializer.errors
+            return Response(data)
+        return Response(data)
+    
+
